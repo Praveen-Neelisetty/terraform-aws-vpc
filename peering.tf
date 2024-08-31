fc8dbeb5cd455e9ff0a2295878ad4b8c1f5719fc
Praveen <saipraveenneelisetty27@gmail.com>
2024-08-31 19:09:09 +0530

    Terraform vpc module with VPC Peering

--- peering.tf
+++ peering.tf
@@ -1,43 +1,44 @@
 ### VPC Peering ###
 
 resource "aws_vpc_peering_connection" "peering" {
-    count = var.is_peering_required ? 1 : 0
-    vpc_id = aws_vpc.main.id # requestor vpc
-    peer_vpc_id = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id  # acceptor vpc
-    auto_accept = var.acceptor_vpc_id == "" ? true : false
+  count       = var.is_peering_required ? 1 : 0
+  vpc_id      = aws_vpc.main.id                                                           # requestor vpc
+  peer_vpc_id = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id # acceptor vpc
+  auto_accept = var.acceptor_vpc_id == "" ? true : false
 
-    tags = merge(
+  tags = merge(
     var.common_tags,
     var.vpc_peering_tags,
     {
-        Name = "${local.resource_name}" #expense-dev
+      Name = "${local.resource_name}" #expense-dev
     }
+  )
 }
 
 resource "aws_route" "public_peering" {
-    count = is_peering_required ? 1 : 0
-    route_table_id = aws_route_table.public.id
-    destination_cidr_block = aws_vpc_peering_connection.peering.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
+  count                     = is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = aws_vpc_peering_connection.peering.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
 
 resource "aws_route" "private_peering" {
-    count = is_peering_required ? 1 : 0
-    route_table_id = aws_route_table.private.id
-    destination_cidr_block = aws_vpc_peering_connection.peering.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id 
+  count                     = is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = aws_vpc_peering_connection.peering.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
 
 resource "aws_route" "database_peering" {
-    count = is_peering_required ? 1 : 0
-    route_table_id = aws_route_table.private.id
-    destination_cidr_block = aws_vpc_peering_connection.peering.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id 
+  count                     = is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = aws_vpc_peering_connection.peering.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
 
 resource "aws_route" "default_peering" {
-    count = is_peering_required ? 1 : 0
-    route_table_id = data.aws_route_table.main.id # default vpc route table
-    destination_cidr_block = var.vpc_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
+  count                     = is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.main.id # default vpc route table
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
